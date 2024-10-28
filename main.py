588c96d35cd5aceaf426f87c7c0adbbd4f939021
sergbondckua <sergninja@gmail.com>
2024-10-28 16:04:36 +0200

    add new template

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ from template_filler import TemplateFiller
 if __name__ == "__main__":
     # Приклад використання
     party_data_src = PartyData(
-        contract_number=f"ОСББ-{datetime.now().replace(day=1).strftime('%d%m%Y')}-105",
-        full_name='ОБ\'ЄДНАННЯ СПІВВЛАСНИКІВ БАГАТОКВАРТИРНОГО БУДИНКУ ЮГОСЛАВСЬКИЙ',
-        short_name='ОСББ "ЮГОСЛАВСЬКИЙ"',
-        address="вулиця Пастерівська, будинок 11",
-        person_name="Розпутня Юлія Іванівна",
-        phone_number="096-404-98-06",
+        contract_number=f"ОСББ-{datetime.now().replace(day=1).strftime('%d%m%Y')}-116",
+        full_name='ЖИТЛОВО-БУДІВЕЛЬНИЙ КООПЕРАТИВ № 55 "ЕФІР 2"',
+        short_name='ЖБК №55 "ЕФІР-2"',
+        address="вулиця Генерала Момота, будинок 15",
+        person_name="Мельник Петро Богданович",
+        phone_number="067-472-38-50",
         city="Черкаси",
-        bank_details="""18016, м.Черкаси, вул.Пастерівська , 11
-р/р UA343052990000026002021604334
-в АТ КБ "Приватбанк"
+        bank_details="""18034, м. Черкаси, вул. Генерала Момота, 15
+IBAN UA583052990000026002011601068
+в АТ КБ «ПриватБанк»
 МФО 305299
-ЄДРПОУ 40726671
-т. 097-507-40-46
+ЄДРПОУ 21368684
+т. 067-472-38-50
 """,
     )
 
-    contract = ContractGenerator(source_price=7100*0.03)
+    contract = ContractGenerator(source_price=50)
     contract_data = contract.get_contract_data(party_data_src)
 
     template_contract_filler = TemplateFiller(
